71318143b16250cf9e411dfbd174ec3de22be66c
유민수 <@DESKTOP-KSABPN4>
2025-01-03 17:34:30 +0900

    feat : w1_01/03

---
+++ src/main/java/w1_4_controllogics/ConditionSample.java
@@ -0,0 +1,48 @@
+package w1_4_controllogics;
+
+import java.util.Objects;
+import java.util.Scanner;
+
+public class ConditionSample {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println("input command : ");
+        String consoleInput = myScanner.nextLine();
+
+        // ##### if #####
+        // 조건문과 조건식
+        // 문(statement) vs 식(expression)
+        // 문 : 동작을 나타내는 코드 -> Return Value 가 없음
+        // 식 : 결과적으로 값을 도출하는 코드 -> Return Value 로 수렴
+        // Return Value 가 있는 경우 변수에 할당 가능해야 함
+        if (consoleInput.equals("true")) {
+            System.out.println("This is true branch!");
+        } else if (consoleInput.equals("middle")) {
+            System.out.println("This is middle branch!");
+        } else {
+            System.out.println("This is else branch!");
+        }
+
+
+        // ##### switch #####
+        // 값의 boolean 변환 없이 분기 수행할 수 있는 구문
+        // 다음 분기로 흐르는 특성이 있기에 break 사용
+        // 아무 분기에 해당하지 않으면 실행할 default 정의 가능
+        // default 에서도 break  명시 권장
+        int commandInt = Integer.parseInt(consoleInput);
+        switch (commandInt) {
+            case 10 :
+                System.out.println("Low Score");
+                break;
+            case 50 :
+                System.out.println("Middle Score");
+                break;
+            case 90 :
+                System.out.println("High Score");
+                break;
+            default:
+                System.out.println("Your score is " + commandInt);
+                break;
+        }
+    }
+}
---
+++ src/main/java/w1_4_controllogics/LoopSample.java
@@ -0,0 +1,48 @@
+package w1_4_controllogics;
+
+public class LoopSample {
+    public static void main(String[] args) {
+        int n = 10;
+        // ##### while #####
+        // 탈출 조건 달성 가능 여부와,
+        // 탈출 조건 달성의 지연 요인을 고려하여 사용
+        while(n>0) {
+            n -= 1;
+            System.out.println(n);
+        }
+
+
+        // ##### do-while #####
+        // 선실행 후판단을 구현하는 방식으로
+        // 탈출 조건 달성 뿐 아니라
+        // 최소한의 코드 실행을 제어하는 구문
+        do {
+            n -= 1;
+            System.out.println(n);
+        } while (n>0);
+
+
+
+        // ##### for ######
+        // 1. 기본 for 문
+        // 횟수 기준 판단이 추가 되기 때문에
+        // 탈출 조건 만족을 검토하기가 비교적 좋다
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+        }
+
+        // 2. 향상된 for 문
+        // 배열을 순회하기 때문에 종료 로직이 안전하며,
+        // 배열 요소값을 돌고 매 회차 로직을 작성할 수 있어서 편리
+        int[] arrayA = {1, 2, 3, 4, 5};
+
+        for (int aa: arrayA) {
+            if (aa == 3) {
+                // continue; // 처음으로 돌아감
+                // break;  // 탈출
+
+            }
+            System.out.println(aa);
+        }
+    }
+}
