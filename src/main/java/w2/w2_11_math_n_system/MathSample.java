27579974282c404f409c39d7d0ecca8417c2d225
유민수 <@DESKTOP-KSABPN4>
2025-01-08 17:57:00 +0900

    feat : 01/08

---
+++ src/main/java/w2/w2_11_math_n_system/MathSample.java
@@ -0,0 +1,15 @@
+package w2.w2_11_math_n_system;
+
+// 빌트인 라이브러리
+import com.sun.security.auth.module.UnixLoginModule;
+
+// 기타 언어 유틸 라이브러리
+import java.util.ArrayList;
+import java.util.Scanner;
+
+// 써드===
+public class MathSample {
+    public static void main(String[] args) {
+
+    }
+}
