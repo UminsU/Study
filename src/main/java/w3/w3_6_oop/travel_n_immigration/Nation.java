e39a5e1771e009fe1bb78fcb4cace43f7614c98b
유민수 <minsoo0515@naver.com>
2025-01-14 17:48:39 +0900

    feat : 01/14

---
+++ src/main/java/w3/w3_6_oop/travel_n_immigration/Nation.java
@@ -0,0 +1,7 @@
+package w3.w3_6_oop.travel_n_immigration;
+
+public class Nation {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/w3/w3_6_oop/travel_n_immigration/People.java
@@ -0,0 +1,7 @@
+package w3.w3_6_oop.travel_n_immigration;
+
+public class People {
+    public static void main(String[] args) {
+
+    }
+}
