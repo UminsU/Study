702288d1f1d64aea39469f5fe342250fd2eef45a
Minsu Yoo <minsoo0515@naver.com>
2025-02-02 18:01:55 +0900

    fix : 데이터 타입 필드 추가 및 수정

--- src/main/java/w3/w3_11_pokemonGame/PokeDex.java
+++ src/main/java/w3/w3_11_pokemonGame/PokeDex.java
@@ -4,43 +4,29 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class PokeDex {
-    // 데이터 타입 추가
+    // 포켓몬 정보 관리할 데이터 타입 필드 추가
+    // 이름으로 검색 시
     static Map<String, Pokemon> pokemonByName = new HashMap<>();
+    // 카테고리를 거쳐서 이름 검색 시
     static Map<PokeCategory, Map<String, Pokemon>> pokemonByCategory = new HashMap<>();
+    // 진화형 검색용 인덱스 Map
+    static Map<String, String> evolveMap = new HashMap<>();
 
-
-    static {
-        // 3. 포켓몬 사전 더미데이터 생성
-        // 모든 PokeCategory 값을 키로 하여 내부 Map 초기화
-        for (PokeCategory category : PokeCategory.values()) {
-            pokemonByCategory.put(category, new HashMap<>());
-        }
-        // 아래에 더미데이터 생성 (매 객체 생성 시 해당 객체로pokemonByNamem pokemonByCategory 둘 다 업데이트 해야함)
-        Pokemon p1 = new Pokemon();
-        Pokemon p2 = new Pokemon();
-        Pokemon p3 = new Pokemon();
-
-        for (int i = 0 ; i<3 ; i++) {
-            //pokemonByName.put(, p1);
-            pokemonByCategory.get(PokeCategory.WATER).put(p1.getCustoName(), p1);
-        }
-
-
+    public static EvolvedPokemon getEvolveForm(String currentForm) {
+        String evolveTo = evolveMap.get(currentForm);
+        return (EvolvedPokemon) pokemonByName.get(evolveTo);
     }
 
     public enum PokeCategory {
-        WATER, FIRE, EARTH, SKY, LEGENDARY, MYSTIC, NORMAL, ELECTRIC
+        MOON,
+        WATER, FIRE, EARTH, SKY,
+        LEGENDARY, MYSTIC, NORMAL, ELECTRIC
     }
+
     public static Pokemon searchPokemon(String name) {
         return pokemonByName.get(name);
-    };
-
+    }
     public static Map<String, Pokemon> searchPokemon(PokeCategory category) {
         return pokemonByCategory.get(category);
-    };
-
-    // 아래 기능 필요없음
-//    public static Pokemon searchPokemon(PokeCategory category, String name) {
-//        return searchPokemon(category).get(name);
-//    }
+    }
 }
