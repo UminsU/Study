6da1ee6a7a4e7fcae60adbfa9b3e049849191eb2
Minsu Yoo <minsoo0515@naver.com>
2025-02-02 18:22:02 +0900

    feat : 생성자 추가

--- src/main/java/w3/w3_11_pokemonGame/SurfPokemon.java
+++ src/main/java/w3/w3_11_pokemonGame/SurfPokemon.java
@@ -1,13 +1,31 @@
 package w3.w3_11_pokemonGame;
 
 public class SurfPokemon extends Pokemon implements ISurfable {
-    @Override
-    public void crossOcean(String tgCity) {
+    public SurfPokemon(String pokemonName, int HP, PokeDex.PokeCategory pokeCategory) {
+        super(pokemonName, HP, pokeCategory);
+    }
 
+    public SurfPokemon(String pokemonName, String customName, int HP, PokeDex.PokeCategory pokeCategory) {
+        super(pokemonName, customName, HP, pokeCategory);
     }
 
     @Override
     public void surf(String tgCity) {
         surf(tgCity);
+    }
+
+    @Override
+    public void surf(PokeTown pokeTown) {
+        System.out.println("Surf to :" + pokeTown.getName());
+    }
+
+    @Override
+    public void crossOcean(String tgCity) {
+        surf(tgCity);
+    }
+
+    @Override
+    public void crossOcean(PokeTown pokeTown) {
+        this.surf(pokeTown);
     }
 }
