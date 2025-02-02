0903fea4d7e3a4fd89464a3a12312f4ecfddbe87
Minsu Yoo <minsoo0515@naver.com>
2025-02-02 18:31:41 +0900

    feat : 생성자 추가

--- src/main/java/w3/w3_11_pokemonGame/LegendPokemon.java
+++ src/main/java/w3/w3_11_pokemonGame/LegendPokemon.java
@@ -3,4 +3,12 @@ package w3.w3_11_pokemonGame;
 import lombok.Getter;
 
 @Getter
-public class LegendPokemon extends Pokemon {}
+public class LegendPokemon extends Pokemon {
+    public LegendPokemon(String pokemonName, int HP, PokeDex.PokeCategory pokeCategory) {
+        super(pokemonName, HP, pokeCategory);
+    }
+
+    public LegendPokemon(String pokemonName, String customName, int HP, PokeDex.PokeCategory pokeCategory) {
+        super(pokemonName, customName, HP, pokeCategory);
+    }
+}
